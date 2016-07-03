430d732f946a673b00a0537dc5d208d58ca32c99
HatemHaddad <hshadad@emirates.net.ae>
2016-07-03 17:24:24 +0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
-test.labels <- read.table("test/y_test.txt",col.names = "label")
-test.subjects <- read.table("test/subject_test.txt", col.names="subject")
-test.data <- read.table("test/X_test.txt")
+
+test.data <- read.table("test/X_test.txt")  # Read Data
+test.labels <- read.table("test/y_test.txt",col.names = "label") # Read labels
+test.subjects <- read.table("test/subject_test.txt", col.names="subject") # Read Subjects
+
