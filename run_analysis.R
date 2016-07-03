fb500d1e98d7b054bd91cef4497d2c8e6195df40
HatemHaddad <hshadad@emirates.net.ae>
2016-07-03 17:36:06 +0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,22 @@
 
+
+# Test Data
+
 test.data <- read.table("test/X_test.txt")  # Read Test Data
 test.labels <- read.table("test/y_test.txt",col.names = "label") # Read Test Labels
 test.subjects <- read.table("test/subject_test.txt", col.names="subject") # Read Test Subjects
+
+# Training Data
 
 train.data <- read.table("train/X_train.txt") # Read Train Data
 train.labels <- read.table("train/y_train.txt", col.names="label") # Read Train Labels
 train.subjects <- read.table("train/subject_train.txt", col.names="subject") # Read Train Subjects
 
+# Binding Test Data - cbind()
+
+test_cbind <- cbind(test.subjects,test.labels,test.data)
+
+# Binding Triaining Data - cbind()
+
+train_cbind <- cbind(train.subjects,train.labels,train.data)
 
