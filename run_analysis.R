0e8a42cc19ad508340ce3aa5b48db7d0f6372a81
HatemHaddad <hshadad@emirates.net.ae>
2016-07-03 17:23:01 +0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,3 @@
-
+test.labels <- read.table("test/y_test.txt",col.names = "label")
+test.subjects <- read.table("test/subject_test.txt", col.names="subject")
+test.data <- read.table("test/X_test.txt")
